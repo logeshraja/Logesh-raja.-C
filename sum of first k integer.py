0dc6eb50f79e45ba7cffbf8093c2914b580ccf28
logeshraja <36471832+logeshraja@users.noreply.github.com>
2018-02-19 17:59:26 +0530

    Create sum of first k integer.py

---
+++ sum of first k integer.py
@@ -0,0 +1,12 @@
+def main():
+    pass
+try:
+	n=int(input())
+	sum=0
+	for i in range(1,n+1):
+		sum=sum+i
+	print(sum)
+except:
+	print("Enter number alone")
+if __name__ == '__main__':
+    main()
