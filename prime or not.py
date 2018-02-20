006fb996de41c4b98fecea12ced510750e3c507f
logeshraja <36471832+logeshraja@users.noreply.github.com>
2018-02-20 17:23:10 +0530

    Create prime or not.py

---
+++ prime or not.py
@@ -0,0 +1,14 @@
+def main():
+    pass
+a=int(input("Enternumber: "))
+k=0
+for i in range(2,a//2+1):
+    if(a%i==0):
+        k=k+1
+if(k<=0):
+    print("Yes")
+else:
+    print("No")
+
+if __name__ == '__main__':
+    main()
